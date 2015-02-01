8ada818ce2b787c91691e943113512dad99b8db3
DPOH-VAR <dpohvar@gmail.com>
2015-02-01 08:59:14 +0500

    clear

--- plugin/src/main/java/ru/dpohvar/varscript/exception/WorkspaceStateException.java
+++
@@ -1,7 +0,0 @@
-package ru.dpohvar.varscript.exception;
-
-/**
- * Exception that is generated when you trying to use unloaded workspace
- */
-public class WorkspaceStateException extends RuntimeException {
-}
